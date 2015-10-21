163b80c40c7d2c526ee97a0138e1eccd9196e4c5
Sally Li <lisally@uw.edu>
2015-10-21 09:59:02 -0700

    second class

--- js/music.js
+++ js/music.js
@@ -43,7 +43,7 @@ $('form').submit(function() {
 
 	// After setting each property, save your new instance back to your database
 	music.save();
-	
+	//getData();
 	return false
 });
 
@@ -52,40 +52,57 @@ $('form').submit(function() {
 // Write a function to get data
 var getData = function() {
 	
-
+	console.log('getData');
 	// Set up a new query for our Music class
-
+	var query = new Parse.Query(Music);
 
 	// Set a parameter for your query -- where the website property isn't missing
-
-
+	//query.exists('website');
+	query.notEqualTo('website', '');
 	/* Execute the query using ".find".  When successful:
 	    - Pass the returned data into your buildList function
 	*/
+	query.find({
+		// success: function(response) {
+		// 	buildList(response);
+		// }
+		success: buildList
+	})
 }
 
 // A function to build your list
 var buildList = function(data) {
+	console.log('buildList');
 	// Empty out your unordered list
-	
+	$('ol').empty();
+
 	// Loop through your data, and pass each element to the addItem function
+	data.forEach(function(d) {
+		addItem(d);
+	});
 
 }
-
 
 // This function takes in an item, adds it to the screen
 var addItem = function(item) {
 	// Get parameters (website, band, song) from the data item passed to the function
-
+	var website = item.get('website');
+	var band = item.get('band');
+	var song = item.get('song');
 	
 	// Append li that includes text from the data item
+	var li = $('ol').append('<li>Check out ' + band + ', I love the song ' + song + '</li>');
 
-
-	
 	// Time pending, create a button that removes the data item on click
-	
+	var button = $('<button class="btn-xs btn-danger">');
+	button.on('click', function() {
+		item.destroy({
+			success: getData
+		})
+	})
+	li.append(button);
+	$('ol').append(li);
 }
 
 // Call your getData function when the page loads
-
 
